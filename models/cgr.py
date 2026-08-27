37aaedd423c6696cd6ea2eeabde5206721889216
Hossein Rezaei <hr505@cl.cam.ac.uk>
2026-08-27 15:45:50 +0330

    Update cgr.py

--- models/cgr.py
+++ models/cgr.py
@@ -171,18 +171,18 @@ class Cgr(ContinualModel):
             
         
             # Sort indices based on the Confidence
-            sorted_indices_1 = np.argsort(Confidence_mean.numpy())
+            ##sorted_indices_1 = np.argsort(Confidence_mean.numpy())
             
             # Sort indices based on the variability
-            ##sorted_indices_2 = np.argsort(Variability.numpy())
+            sorted_indices_2 = np.argsort(Variability.numpy())
             
         
-            top_indices_sorted = sorted_indices_1 #hard
+            ##top_indices_sorted = sorted_indices_1 #hard
             
             ##top_indices_sorted = sorted_indices_1[::-1].copy() #simple
         
             # Descending order
-            ##top_indices_sorted = sorted_indices_2[::-1].copy() #challenging
+            top_indices_sorted = sorted_indices_2[::-1].copy() #challenging
 
 
             # Initialize lists to hold data
